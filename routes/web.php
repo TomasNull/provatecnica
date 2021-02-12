43b4cb698934ad9eb870f631a3d1f6766b5c89a0
TomasNull <moreni.tomas@gmail.com>
2021-02-12 15:40:35 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('login/{driver}', 'Auth\LoginController@redirectToProvider')->name('social_auth');
+Route::get('login/{driver}/callback', 'Auth\LoginController@handlerProviderCallback');
 
 Route::get('/', function () {
     return view('welcome');
